547a257738b12bc84c4b8c84346eb78ca003c633
Vanilda Alexandra Jaguar Saw <vanildasaw@Vanildas-MacBook-Air.local>
2026-03-13 10:50:14 +0200

    conexao via variaveis de ambiente

--- conexao.php
+++ conexao.php
@@ -1,11 +1,11 @@
 <?php
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
+$port = (int)getenv('DB_PORT');
 
-$host = "sql103.infinityfree.com";
-$user = "if0_41339057";
-$pass = "alphasolution26";
-$db   = "if0_41339057_alphasolutions";
-
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Erro de conexão: " . $conn->connect_error);
